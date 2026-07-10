912da007a7a783118678b0b9f380acb3ebc2b7b9
chetan1804 <chetan.shinde@cyncly.com>
2026-07-10 16:05:34 +0530

    Loops in python

---
+++ loops.py
@@ -0,0 +1,33 @@
+"""Loops in Python"""
+"""1) for loop"""
+# print("Even numbers from 1 to 100:")
+# for i in range(1,101):
+#   if(i%2==0):
+#     print(i)
+
+# print("Revers numbers from 100 to 1:")
+# for i in range(100, 0, -1):
+#     print(i)
+
+# print("Sum of numbers from 1 to 10:")
+# total = 0
+# for i in range(1, 11):
+#     total += i
+# print(total)
+
+"""2) while loop"""
+# i=1
+# while(i<=10):
+#   print(i)
+#   i+=1
+
+# i= 1
+# while (i<=10):
+#   if(i==5):
+#     i+=1
+#     continue
+#   if(i==8):
+#     i+=2
+#     pass
+#   print(i)
+#   i+=1
