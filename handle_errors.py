ef860237982ef78416b9ddd2f8ba9b4b0b7471f7
chetan1804 <chetanshinde265@gmail.com>
2026-07-12 08:05:36 +0530

    Error handling in python

---
+++ handle_errors.py
@@ -0,0 +1,28 @@
+# try:
+#     print(10/a)
+
+# except Exception as err:
+#     print(f"sorry there is an err as {err}")
+
+# else:
+#     print("good there is no exception")
+
+# finally:
+#     print("i will run no matter what")
+
+# print("ok i have done the division")
+
+
+# age = int(input("tell your age :- "))
+
+# try:
+
+#     if age < 10 or age > 18:
+#         raise ValueError("your age must be between 10 and 18")
+#     else:
+#         print("welcome to the club")
+
+# except Exception as err:
+#     print(f"an error occured as {err}")
+
+# print("the club will start soon")
