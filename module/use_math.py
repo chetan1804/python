d57cc54296c4f53bcb5a95a64bc0afe952d83d42
chetan1804 <chetanshinde265@gmail.com>
2026-07-23 06:50:20 +0530

    Python concepts - Modules and packages

---
+++ module/use_math.py
@@ -0,0 +1,7 @@
+import math_utils
+import text_utils as tu
+
+print(math_utils.add(5, 3))  # Output: 8
+print(math_utils.multiply(4, 3))  # Output: 12
+print(math_utils.PI)  # Output: 3.14159
+print(tu.print_text("Hello, World!"))  # Output: Hello, World!
---
+++ package/use_packages.py
@@ -0,0 +1,8 @@
+# import math_pk.add as add
+# import math_pk.multiply as multiply
+
+from math_pk import add, multiply
+
+
+print("Addition of 5 and 3:", add.add(5, 3))
+print("Multiplication of 5 and 3:", multiply.multiply(5, 3))
